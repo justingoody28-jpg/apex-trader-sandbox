1bbe556ad41a23babe368c55a9411723d3b89565
justingoody28-jpg <justingoody28@gmail.com>
2026-03-24 16:19:19 -0500

    fix: force unique build ID to bust Vercel compiled chunk cache

--- next.config.js
+++ next.config.js
@@ -1,5 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: false,
+  generateBuildId: async () => {
+    // Force unique build ID to prevent Vercel from restoring stale compiled chunks
+    return 'build-' + Date.now();
+  },
 }
 module.exports = nextConfig
